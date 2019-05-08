bb238efe1ee29a83e7f740532ff0826b6d672a83
Darren <491361994@qq.com>
2019-05-08 19:24:29 +0800

    解构赋值

--- app/js/class/lesson2.js
+++ app/js/class/lesson2.js
@@ -18,3 +18,19 @@
   [a, b, c = 3] = [1, 2];
   console.log(a, b, c);
 }
+{
+  function f() {
+    return [1, 2];
+  }
+  let a, b;
+  [a, b] = f();
+  console.log(a, b);
+}
+{
+  function f() {
+    return [1, 2, 3, 4, 5];
+  }
+  let a, b, c;
+  [a, ...b] = f();
+  console.log(a, b);
+}
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -61,6 +61,10 @@
 
 	"use strict";
 
+	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+	function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
+
 	{
 	  var a = void 0,
 	      b = void 0,
@@ -100,6 +104,41 @@
 	  c = _ref$ === undefined ? 3 : _ref$;
 
 	  console.log(_a3, _b3, c);
+	}
+	{
+	  var f = function f() {
+	    return [1, 2];
+	  };
+
+	  var _a4 = void 0,
+	      _b4 = void 0;
+
+	  var _f = f();
+
+	  var _f2 = _slicedToArray(_f, 2);
+
+	  _a4 = _f2[0];
+	  _b4 = _f2[1];
+
+	  console.log(_a4, _b4);
+	}
+	{
+	  var _f3 = function _f3() {
+	    return [1, 2, 3, 4, 5];
+	  };
+
+	  var _a5 = void 0,
+	      _b5 = void 0,
+	      _c = void 0;
+
+	  var _f4 = _f3();
+
+	  var _f5 = _toArray(_f4);
+
+	  _a5 = _f5[0];
+	  _b5 = _f5.slice(1);
+
+	  console.log(_a5, _b5);
 	}
 
 /***/ })

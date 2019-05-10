2e1f41e7eb8b18164eb664327b80ffe81932883b
Darren <491361994@qq.com>
2019-05-10 22:03:14 +0800

    数值扩展

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,6 @@
 // import "./class/lesson1.js";
 // import "./class/lesson2.js";
 // import "./class/lesson3.js";
-import "./class/lesson4.js";
+// import "./class/lesson4.js";
+// import "./class/lesson5.js";
+import "./class/lesson6.js";

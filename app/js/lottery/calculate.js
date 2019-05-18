e85b77a1307f4766b1ef4440010d99205f7a8fde
Darren <491361994@qq.com>
2019-05-18 22:23:29 +0800

    calculate

---
+++ app/js/lottery/calculate.js
@@ -0,0 +1,13 @@
+class Calculate {
+  /**
+   * [computeCount] 计算注数
+   * @param {number} active [当前选中的号码]
+   * @param {string} play_name [当前的玩法标志]
+   * @param {number}
+   */
+  computeCount(active ,play_name) {
+
+  }
+}
+
+export default Calculate;

066b88e7b0527037d7b2e983d033f30ac568503c
Darren <491361994@qq.com>
2019-05-22 21:02:05 +0800

    Calculate

--- app/js/lottery/calculate.js
+++ app/js/lottery/calculate.js
@@ -5,8 +5,45 @@ class Calculate {
    * @param {string} play_name [当前的玩法标志]
    * @param {number}
    */
-  computeCount(active ,play_name) {
+  computeCount(active, play_name) {
+    let count = 0;
+    const exist = this.play_list.has(play_name);
+    const arr = new Array(active).fill('0');
+    if (exist && play_name.at(0) === 'r') {
+      count = Calculate.combine(arr, play_name.split('')[1]);
+    }
+    return count;
+  }
 
+  /**
+   * [combine 组合运算]
+   * @param {Array} arr [参与组合运算的数组]
+   * @param {number} size [组合运算的基数]
+   * @param {number}      [计算注数]
+   */
+  static combine(arr, size) {
+    let allResult = [];
+    (function f(arr, size, result) {
+      let arrLen = arr.length;
+      if (size > arrLen) {
+        return;
+      }
+      if (size === arrLen) {
+        allResult.push([].concat(result, arr))
+      } else {
+        for (let i = 0; i < allLen; i++) {
+          let newResult = [].concat(result);
+          newResult.push(arr[i]);
+          if (size === 1) {
+            allResult.push(newResult);
+          } else {
+            let newArr = [].concat(arr);
+            newArr.splice(0, i + 1);
+            f(newArr, size - 1, newResult);
+          }
+        }
+      }
+    })(arr, size, [])
   }
 }
 
